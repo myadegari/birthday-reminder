342a61a172b7da878ab5428c6536d7a575e8be7d
Mehyad <43511859+myadegari@users.noreply.github.com>
2024-05-06 12:49:02 +0330

    edit config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,8 @@
 import { defineConfig } from 'vite'
-import { fileURLToPath, URL } from 'url'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  }
+ 
 })
